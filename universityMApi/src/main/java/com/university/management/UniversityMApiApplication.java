ddf4c43e18372e9caa53f9c77071358a6a7d3e95
NitishKumarNirala <nitishkumarnirala1992@gmail.com>
2019-11-16 13:32:05 +0530

    added university setup project

---
+++ universityMApi/src/main/java/com/university/management/UniversityMApiApplication.java
@@ -0,0 +1,13 @@
+package com.university.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMApiApplication.class, args);
+	}
+
+}
